69f6de9d3a450e4f897a74271aa7c5f84e93e877
Adham Abdelfattah <50050146+adhamali74@users.noreply.github.com>
2021-05-09 23:34:01 +0200

    +

--- app/src/main/java/com/example/edmt/ui/about_us/About_us_Fragment.kt
+++ app/src/main/java/com/example/edmt/ui/about_us/About_us_Fragment.kt
@@ -6,28 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.edmt.R
-import com.example.edmt.ui.edit_info.Edit_info_ViewModel
 
 class About_us_Fragment : Fragment() {
 
-
-    private lateinit var aboutUsViewmodel: About_Us_ViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        aboutUsViewmodel =
-            ViewModelProvider(this).get(About_Us_ViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_aboutus, container, false)
         val textView: TextView = root.findViewById(R.id.text_about_us)
-        aboutUsViewmodel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 }
